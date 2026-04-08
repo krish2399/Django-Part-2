24676165e9d3a126c28f9015231f49a3fb7a2e6b
krish2399 <krish.nivzen@gmail.com>
2026-04-08 10:19:15 +0530

    Building RESTful APIs with Django REST Framework

---
+++ store/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from decimal import Decimal
+from .models import Product
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=20)
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price_with_text = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
