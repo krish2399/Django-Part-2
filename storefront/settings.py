a71f0312ce20d62f778224aec9b1d7f0a21704e4
krish2399 <krish.nivzen@gmail.com>
2026-04-13 15:05:09 +0530

    Designing and Implementing a Shopping Cart API complate

--- storefront/settings.py
+++ storefront/settings.py
@@ -92,11 +92,8 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront2',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Krish@2004'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
